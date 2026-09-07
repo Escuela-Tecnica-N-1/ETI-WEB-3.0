81052cf07acbfe695170eeb95097df08e9e74dc6
Tomi <urratomi002@gmail.com>
2026-09-07 09:38:39 -0300

    Cambié el schema del usuario para que sea compatible con las verificaciones

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,14 @@
-const { Schema, model, SchemaType} = require('mongoose');
+const { Schema, model, SchemaType } = require('mongoose');
 
-const userSchema = new Schema ({
-    nombre: {type: String, required: true},
-    email: {type: String, required: true},
-    contrasena: {type: String, required: true},
-    roles: [{type: String, enum: ['admin', 'profesor', 'alumno'], default: ['alumno'] }]
+const userSchema = new Schema({
+    nombre: { type: String, required: true },
+    email: { type: String, required: true },
+    contrasena: { type: String, required: true },
+    roles: [{ type: String, enum: ['admin', 'profesor', 'alumno'], default: ['alumno'] }],
+    verificado: { type: Boolean, default: false },
+    tokenVerificacion: { type: String },
+    tokenVerificacionExpira: { type: Date }
 });
 
-const User = model ('User', userSchema);
+const User = model('User', userSchema);
 module.exports = User;
