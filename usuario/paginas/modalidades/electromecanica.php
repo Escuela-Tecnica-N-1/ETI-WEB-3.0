4f68d759533caac5372220077c0c036812ed484e
Juan busqued <busquedjuan@gmail.com>
2026-09-15 15:53:23 -0300

    commit de guardado para probar cosas

--- usuario/paginas/modalidades/electromecanica.php
+++ usuario/paginas/modalidades/electromecanica.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Técnico en Electromecánica - Escuela Técnica</title>
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/electromecanica.css">
+    <link rel="stylesheet" href="../../css/electromecanica.css">
     <link rel="icon" href="imagenes/escudo.png" type="image/png">
     <!-- Font Awesome for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
