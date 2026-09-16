ac3526b03679b5e381a4e39ffd9c0401f7381135
Juan busqued <busquedjuan@gmail.com>
2026-09-16 11:07:27 -0300

    acomodando links

--- usuario/paginas/autoridades/autoridades.php
+++ usuario/paginas/autoridades/autoridades.php
@@ -4,20 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Autoridades</title>
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/autoridades.css">
+    <link rel="stylesheet" href="css\styles.css">
+    <link rel="stylesheet" href="autoridades.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
     <link rel="icon" href="imagenes/escudo.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
-<?php include 'header.php'; ?>
+
 <body>
 
-    <?php include '404.php'; ?>
+    <?php include '../componentes/header.php'; ?>
+    <?php include '../componentes/navbar.php'; ?>
+    <?php include '../componentes/404.php'; ?>
+    <?php include '../componentes/footer.php'; ?>
 
 </body>
-<?php include 'footer.php'; ?>
+
 </html>
